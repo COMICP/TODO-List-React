a3bfac8569a351d1fefb638007ce6004da4183d4
COMICP <binettejaden@gmail.com>
2025-05-10 18:12:43 -0400

    add items start

--- todo list/src/App.jsx
+++ todo list/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./style.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [newitem, setNewItem] = useState("")
+  const[todos, setTodos] = useState([])
+  function handleSub(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return[
+        ...currentTodos,
+        {id: crypto.randomUUID(), title: newitem, completed: false},
+      ]
+    })
+  }
+
+  return <>
+  <form onSubmit={handleSub} className = "new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input value={newitem}
+      onChange={e => setNewItem(e.target.value)} 
+      type="text" id="item" />
+    </div>
+    <button className="butt">add</button>
+  </form>
+  <h1 className="Header">Todo List</h1> 
+  <ul className="list">
+    {todos.map(todo=> {
+      return(
+        <li key={todo.id}>
+          <label htmlFor="">
+            <input type="checkbox" checked={todo.completed} />
+            {todo.title}
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      )
+    })}
+  </ul>
+  </>
 }
-
-export default App
